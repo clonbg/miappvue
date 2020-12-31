1e5536ae4deb846dbfe399c7a3ec49c3749e6d58
clonbg <clonbg@gmail.com>
2020-12-31 19:29:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>yeeeeeeeeeeeee:{{ contador }}</p>
+    <button @click="sumarMasUno">Más uno</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {
+    sumarMasUno() {
+      this.contador++;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
