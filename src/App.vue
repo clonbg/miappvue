8f52a947fe33e8d24223e2dac06924293b4b9a72
clonbg <clonbg@gmail.com>
2020-12-31 19:34:00 +0100

    format

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <p>yeeeeeeeeeeeee:{{ contador }}</p>
-    <button @click="sumarMasUno">Más uno</button>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<p>yeeeeeeeeeeeee:{{ contador }}</p>
+		<button @click="sumarMasUno">Más uno</button>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+	import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
@@ -29,12 +29,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
